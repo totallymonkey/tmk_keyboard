935153d1920feccda3ca08a6918e3fa308b39ea7
Evan Sailer <esailer@asu.edu>
2016-08-26 21:44:33 -0700

    added roadkit firmware

---
+++ keyboard/roadkit/keymap_roadkit.c
@@ -0,0 +1,26 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* 0: qerty */
+    KEYMAP(KP_7,  KP_8,   KP_9,   KP_PLUS, \
+           KP_4,  KP_5,   KP_6,   KP_MINUS, \
+           KP_1,  KP_2,   KP_3,   KP_ENTER, \
+           KP_0,  KP_DOT, FN0,    BSPC ),
+
+    /* 1: FN 1 */
+    KEYMAP(NUMLOCK,  FN1,   TRNS,   VOLU, \
+           TRNS,  UP,   TRNS,   VOLD, \
+           LEFT,  DOWN,   RIGHT,   TRNS, \
+           TRNS,  TRNS,   TRNS, TRNS ),
+
+   /* 2: FN 2 */
+   KEYMAP(TRNS,  TRNS,   TRNS,   TRNS, \
+          TRNS,  TRNS,   TRNS,   TRNS, \
+          TRNS,  TRNS,   TRNS,   TRNS, \
+          TRNS,  TRNS,   TRNS, TRNS ),
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+    [0] = ACTION_LAYER_TOGGLE(1),
+    [1] = ACTION_LAYER_TOGGLE(2),
+};
