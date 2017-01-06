6d24652787f9ea3816e74b9b795aaa153c0f128d
Evan Sailer <esailer@ip-10-157-200-108.us-west-2.compute.internal>
2017-01-06 16:18:34 -0700

    fixed config

--- keyboard/minorca/config.h
+++ keyboard/minorca/config.h
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 #define MATRIX_ROWS 4
-#define MATRIX_COLS 13
+#define MATRIX_COLS 12
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
