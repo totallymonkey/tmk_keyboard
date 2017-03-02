89ae3fa4f8d4a95067dcdeb71508bc13208a80d4
Evan Sailer <esailer@asu.edu>
2017-03-01 22:10:51 -0600

    update command keymap

--- keyboard/low-writer/keymap_common.h
+++ keyboard/low-writer/keymap_common.h
@@ -64,7 +64,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-    K30, K31, K32, K33, K34,      K37, K38,      K39, K3A, K3B  \
+    K30, K31, K32, K33, K34,      K37, K38,      K39,      K3B  \
 ) { \
     { KC_##K00, KC_##K01, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07, KC_##K08, KC_##K09, KC_##K0A, KC_##K0B }, \
     { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19, KC_##K1A, KC_##K1B }, \
