32a55ad369b297a5d0e94ea82d480272d82d3a60
Andrew Davis <andrewnd@gmail.com>
2017-04-18 20:52:19 -0700

    Updating layout

--- keyboard/tv44-rev3/keymap_tv44.c
+++ keyboard/tv44-rev3/keymap_tv44.c
@@ -3,20 +3,20 @@
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    /* Layer: Base Layer
     * ,-------------------------------------------------.
-    * |Tab|  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P| BSp |
+    * |Tab|  Q|  W|  F|  P|  B|  J|  L|  U|  Y|  ;| BSp |
     * |-------------------------------------------------|
-    * |FN1 |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|Fn1'|
+    * |FN1 |  A|  R|  S|  T|  G|  H|  N| E | I|  O|Fn1'|
     * |-------------------------------------------------|
-    * |SftEs|  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|FN2|
+    * |SftEs|  Z|  X|  C|  D|  V|  K|  M|  ,|  .|  /|FN2|
     * |-------------------------------------------------|
     * |Ctrl|  FN2  |Gui |Enter| Space |Alt |Shift|  FN3 |
     * `-------------------------------------------------'
     */
    KEYMAP(
-     TAB ,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,  BSPC, \
-     FN0  ,   A,   S,   D,   F,   G,   H,   J,   K,   L,SCLN, FN21, \
-     FN22  ,   Z,   X,   C,   V,   B,   N,   M,COMM, DOT,SLSH, FN1, \
-     LCTL,  FN1,  LGUI,     ENT,       SPC,   RALT,   RSFT,  FN2    ),
+     TAB ,   Q,   W,   F,   P,   B,   J,   L,   U,   Y,   SCLN,  BSPC, \
+     FN0  ,   A,   R,   S,   T,   G,   H,   N,   E,  I, O, FN21, \
+     FN22  ,   Z,   X,   C,   D,   V,   K,   M,COMM, DOT,SLSH, FN1, \
+     LCTL,  FN1,  LALT,     FN23,       SPC,   RGUI,   RSFT,  FN2    ),
 
    /* Layer: Function Layer 1
     * ,-------------------------------------------------.
@@ -31,9 +31,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
    KEYMAP(
      GRV , FN4, FN5, FN6, FN7, FN8, FN9,FN10,FN11,FN12,FN13,   DEL, \
-     TRNS ,BSLS,QUOT,MINS, EQL,LBRC,RBRC,DOWN,  UP,LEFT,RGHT, TRNS, \
-     TRNS  , ESC,FN20,PSCR,TRNS,TRNS,TRNS,MSTP,MPLY,MPRV,MNXT,RSFT, \
-     TRNS, LGUI,  TRNS,    TRNS,      TRNS,   TRNS,   TRNS,  TRNS   ),
+     TRNS ,BSLS,QUOT,MINS, EQL,LBRC,RBRC,LEFT,  DOWN,UP,RIGHT, TRNS, \
+     TRNS  , ESC,TRNS,PSCR,TRNS,TRNS,TRNS,HOME,PGDN,PGUP,END,RSFT, \
+     TRNS, TRNS,  TRNS,    TRNS,      TRNS,   TRNS,   TRNS,  BTLD   ),
 
    /* Layer: Function Layer 2
     * ,-------------------------------------------------.
@@ -50,9 +50,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      FN3 ,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0,  TRNS, \
      ESC  ,FN14,FN15,FN16,FN17,FN18,FN19,   4,   5,   6,VOLU,  ENT, \
      TRNS  ,TRNS,TRNS,TRNS,TRNS,TRNS,   0,   1,   2,   3,VOLD,TRNS, \
-     TRNS, TRNS,  TRNS,    TRNS,      TRNS,   TRNS,   TRNS,  TRNS   ),
+     TRNS, TRNS,  TRNS,    TRNS,      0,   TRNS,   TRNS,  TRNS   ),
 
-   /* Layer: Game Layer
+   /* Layer: Game Layer (3)
     * ,-------------------------------------------------.
     * |   |   |   |   |   |   |   | F1| F2| F3| F4|     |
     * |-------------------------------------------------|
@@ -64,10 +64,27 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * `-------------------------------------------------'
     */
    KEYMAP(
-     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  F1,  F2,  F3,  F4,  TRNS, \
-     ESC  ,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  F5,  F6,  F7,  F8,  ENT, \
+     TRNS,Q,W,E,R,T,Y,  F1,  F2,  F3,  F4,  TRNS, \
+     ESC  ,A,S,D,F,G,TRNS,  F5,  F6,  F7,  F8,  ENT, \
      LSFT  ,   1,   2,   3,   4,   5,   6,  F9, F10, F11, F12,RSFT, \
      TRNS, LSFT,     B,     SPC,         C,   TRNS,   TRNS,  TRNS  ),
+	 
+	/* Layer: Function Layer 4 (SpaceFN)
+    * ,-------------------------------------------------.
+    * |  `|  !|  @|  #|  $|  %|  ^|  &|  *|  (|  )| Del |
+    * |-------------------------------------------------|
+    * |    |  \|  '|  -|  =|  [|  ]| Dn| Up|Lft| Rt|    |
+    * |-------------------------------------------------|
+    * |     |Esc|LED|PSc|   |   |   |Stp|Ply|Prv|Nxt|Sft|
+    * |-------------------------------------------------|
+    * |    |  Gui  |    |     |       |    |     |      |
+    * `-------------------------------------------------'
+    */
+   KEYMAP(
+     TRNS , TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,FN18,FN19,TRNS,TRNS,   TRNS, \
+     TRNS ,TRNS,TRNS,TRNS, TRNS,TRNS,TRNS,FN12,FN13,TRNS,TRNS, TRNS, \
+     TRNS  , TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,LBRC,RBRC,TRNS,TRNS,TRNS, \
+     TRNS, TRNS,  TRNS,    TRNS,      TRNS,   TRNS,   TRNS,  TRNS   ),
 };
 
 const action_t PROGMEM fn_actions[] = {
@@ -93,5 +110,6 @@ const action_t PROGMEM fn_actions[] = {
     [19] = ACTION_MODS_KEY(MOD_LSFT, KC_RBRC),
     [20] = ACTION_BACKLIGHT_STEP(),
     [21] = ACTION_LAYER_TAP_KEY(1, KC_QUOT),
-    [22] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_ESC)
+    [22] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_ESC),
+	[23] = ACTION_LAYER_TAP_KEY(4, KC_ENTER)
 };
